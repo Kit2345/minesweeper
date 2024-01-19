cf36b2d8423293a069afa3302cd52e86801fb807
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-19 17:43:36 +0000

    layout of game

---
+++ main.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector(".grid");
+const endGameScreen = document.querySelector(".end-game-screen");
+
+// Making game board
+console.log("Hello");
+console.log(grid);
+console.log(endGameScreen);
