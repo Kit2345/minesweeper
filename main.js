829910aca57e0173dcb1a492e2ba50e8e0cfcc1d
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-22 17:40:06 +0000

    bomb locations function done

--- main.js
+++ main.js
@@ -3,7 +3,21 @@ const grid = document.querySelector(".grid");
 const endGameScreen = document.querySelector(".end-game-screen");
 
 // Game variables
+let currentNumbOfBombs = 1;
 const numbOfBombs = 90;
+const bombsArray = [0];
+
+// Random generator of bomb locations
+while (currentNumbOfBombs < numbOfBombs) {
+  const randomNumber = Math.floor(Math.random() * 100);
+  if (!bombsArray.includes(randomNumber)) {
+    bombsArray.push(randomNumber);
+    currentNumbOfBombs++;
+  }
+}
+
+console.log(bombsArray);
+console.log(bombsArray.length);
 
 // Making game board
 console.log("Hello");
