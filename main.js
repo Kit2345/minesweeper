422f5f48a9231a1bc1a57263bfa6241f2d057d9c
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-22 17:18:26 +0000

    cell clicked function added

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
+// Query selectors
 const grid = document.querySelector(".grid");
 const endGameScreen = document.querySelector(".end-game-screen");
+
+// Game variables
+const numbOfBombs = 90;
 
 // Making game board
 console.log("Hello");
 console.log(grid);
 console.log(endGameScreen);
 
-// Create grid
+function cellClicked() {
+  this.classList.add("cell-clicked");
+}
+
 for (let i = 0; i < 100; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
+  cell.addEventListener("click", cellClicked);
+
   grid.appendChild(cell);
 }
