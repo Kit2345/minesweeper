45854af670b7110f6f89662c08fba54cb08ae6c1
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-22 16:57:54 +0000

    bomb image added

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ console.log("Hello");
 console.log(grid);
 console.log(endGameScreen);
 
+// Create grid
 for (let i = 0; i < 100; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
