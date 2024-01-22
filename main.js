5e5c0efca39bdb21dd43b8670af12bf70c4ca5d8
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-01-22 18:48:42 +0000

    added show all bombs function at game over

--- main.js
+++ main.js
@@ -9,14 +9,19 @@
 // Query selectors
 const grid = document.querySelector(".grid");
 const endGameScreen = document.querySelector(".end-game-screen");
+const playAgainBtn = document.querySelector(".play-again");
+const scoreDisplay = document.querySelector(".score");
+const endGameText = document.querySelector(".end-game-text");
 
 // Game variables
-let currentNumbOfBombs = 1;
-const numbOfBombs = 90;
-const bombsArray = [0];
+let currentNumbOfBombs = 0;
+const totalNumbOfBombs = 5;
+const bombsArray = [];
+let currentScore = 0;
+const winningScore = 5;
 
 // Random generator of bomb locations
-while (currentNumbOfBombs < numbOfBombs) {
+while (currentNumbOfBombs < totalNumbOfBombs) {
   const randomNumber = Math.floor(Math.random() * 100);
   if (!bombsArray.includes(randomNumber)) {
     bombsArray.push(randomNumber);
@@ -24,29 +29,61 @@ while (currentNumbOfBombs < numbOfBombs) {
   }
 }
 
-console.log(bombsArray);
-console.log(bombsArray.length);
+// console.log(bombsArray);
+// console.log(bombsArray.length);
 
 // Making game board
-console.log("Hello");
-console.log(grid);
-console.log(endGameScreen);
+// console.log("Hello");
+// console.log(grid);
+// console.log(endGameScreen);
 
-function gameOver() {
+function gameOver(isVictory) {
+  if (isVictory) {
+    endGameScreen.classList.add("win");
+    endGameText.innerText = "You Won!";
+  }
+  showAllBombs();
   endGameScreen.classList.remove("hidden");
+}
+
+function showAllBombs() {
+  for (let i = 0; i < 100; i++)
+    if (bombsArray.includes(i)) {
+      let cell = document.querySelector(`.num-${i}`);
+      cell.classList.add("cell-bomb");
+      cell.classList.add("cell-clicked-bomb");
+    }
+}
+
+function updateScore() {
+  currentScore++;
+  scoreDisplay.innerText = currentScore.toString().padStart(5, "0");
+  if (currentScore === winningScore) {
+    gameOver(true);
+  }
 }
 
 for (let i = 0; i < 100; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
+  cell.classList.add(`num-${i}`);
   cell.addEventListener("click", function () {
+    // cell clicked has bomb
     if (bombsArray.includes(i)) {
       cell.classList.add("cell-bomb");
       cell.classList.add("cell-clicked-bomb");
-      gameOver();
+      gameOver(false);
     }
-    cell.classList.add("cell-clicked");
+    // Cell clicked doesnt have bomb
+    else {
+      cell.classList.add("cell-clicked");
+      updateScore();
+    }
   });
 
   grid.appendChild(cell);
 }
+
+playAgainBtn.addEventListener("click", function () {
+  location.reload();
+});
